3d6895cbdaa88116fa084f3c30232901cb32e3f8
Gregor Pintar <grpintar@gmail.com>
2013-05-14 07:58:47 +0200

    ofb mode header added

---
+++ include/kripto/mode/ofb.c
@@ -0,0 +1,17 @@
+#ifndef KRIPTO_MODE_OFB_H
+#define KRIPTO_MODE_OFB_H
+
+#include <kripto/mode.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern kripto_mode_desc *const kripto_mode_ofb;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
