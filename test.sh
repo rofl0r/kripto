20307ac55594b73920ef0a0e982864e54bd4879d
Gregor Pintar <grpintar@gmail.com>
2013-07-05 20:20:56 +0200

    blowfish: support for up to 34 rounds

--- test.sh
+++ test.sh
@@ -9,14 +9,14 @@ CFLAGS="libkripto.a -std=c99 -pedantic -Wall -Wextra -Wstrict-prototypes -Wmissi
 #cc test/block/xtea.c $CFLAGS -o t
 #./t
 
-#cc test/block/blowfish.c $CFLAGS -o t
-#./t
+cc test/block/blowfish.c $CFLAGS -o t
+./t
 
 #cc test/block/serpent.c $CFLAGS -o t
 #./t
 
-cc test/block/camellia.c $CFLAGS -o t
-./t
+#cc test/block/camellia.c $CFLAGS -o t
+#./t
 
 #cc test/block/threefish.c $CFLAGS -o t
 #./t
