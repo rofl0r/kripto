25341a24e7db5746a5181d0f39cfb3a64c501d32
Gregor Pintar <grpintar@gmail.com>
2013-12-06 20:18:36 +0100

    "crayon" license replaced with CC0 waiver

--- perf/perf.h
+++ perf/perf.h
@@ -1,3 +1,16 @@
+/*
+ * Written in 2013 by Gregor Pintar <grpintar@gmail.com>
+ *
+ * To the extent possible under law, the author(s) have dedicated
+ * all copyright and related and neighboring rights to this software
+ * to the public domain worldwide.
+ * 
+ * This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
